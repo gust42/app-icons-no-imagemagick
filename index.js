fb44f49a94b6e2201641533126a27d0b2d7aee9f
Gusten Andersson <gusten.andersson@momentum.se>
2019-05-16 10:18:18 +0200

    Use chaining to create one image at a time

--- index.js
+++ index.js
@@ -4,145 +4,135 @@ config = require("./config.json"),
 	path = require('path');
 extend = require('util')._extend;
 
-function createIcons(image, options) {
-	return new Promise(function (resolve, reject) {
-		for (var i in config.icons) {
-			var icon = config.icons[i];
-			var tmpImg = image.clone();
-			tmpImg.resize(icon.width, icon.height).write(options.targetPath + "/" + icon.platform + "/icon/" + icon.name + ".png", function (err) {
-				if (err) {
-					handleError(err, options);
-					reject();
-				}
-			});
+function createIcons(image, doneResolve, options) {
+
+	if (config.icons.length == 0) {
+		doneResolve();
+		return;
+	}
+
+	var icon = config.icons.pop();
+	var tmpImg = image.clone();
+	tmpImg.resize(icon.width, icon.height).write(options.targetPath + "/" + icon.platform + "/icon/" + icon.name + ".png", function (err) {
+		if (err) {
+			handleError(err, options);
+		} else {
 			if (options.progressCB)
 				options.progressCB(icon.name);
-			//console.log(icon.name+" created!");
+			createIcons(image, doneResolve, options);
 		}
-		resolve();
 	});
+	//console.log(icon.name+" created!");
 }
 
-function createSplash(image, options) {
-	console.log('Creating splash');
-	return new Promise(function (resolve, reject) {
-		var promises = [];
-		for (var i in config.splash) {
-			var splash = config.splash[i];
+function createSplash(image, doneResolve, options) {
+	// console.log('Creating splash');
+	if (config.splash.length == 0) {
+		doneResolve(image);
+		return;
+	}
 
-			promises.push(new Promise(function (resolve, reject) {
-				new Jimp(splash.width, splash.height, 0xFFFFFFFF, function (err, bg) {
-					if (err) handeError(err);
-	
-					var platform = "ios";
-					if (splash.platform)
-						platform = splash.platform
-	
-					var pos = getCenter(bg.bitmap, image.bitmap);
-					bg.composite(image, pos.x, pos.y).write(options.targetPath + "/" + splash.platform + "/splash/" + splash.name + ".png", function (err) {
-						if (err) {
-							handleError(err, options);
-							reject();
-						}
-					});
-	
-					if (options.progressCB)
-						options.progressCB(splash.name);
-					//console.log(splash.name+" created!");
-					resolve();
-				});
-			}));
-			
+	var splash = config.splash.pop();
 
-		}
-		Promise.all(promises).then(function () {
-			resolve();
-		});
-	});
-}
+	new Jimp(splash.width, splash.height, 0xFFFFFFFF, function (err, bg) {
+		if (err) handeError(err);
 
-function createMisc(image, options) {
-	return new Promise(function (resolve, reject) {
-		var promises = [];
-		for (var i in config.misc) {
-			var misc = config.misc[i];
+		var platform = "ios";
+		if (splash.platform)
+			platform = splash.platform;
 
-			promises.push(new Promise(function (resolve, reject) {
-				new Jimp(misc.width, misc.height, 0xFFFFFFFF, function (err, bg) {
-
-					if (err) handeError(err);
-					var pos = getCenter(bg.bitmap, image.bitmap);
-					bg.composite(image, pos.x, pos.y).write(options.targetPath + "/misc/" + misc.name + ".png", function (err) {
-						if (err) {
-							handleError(err, options);
-							reject();
-						}
-					});
-	
-					if (options.progressCB)
-						options.progressCB(misc.name);
-					//console.log(splash.name+" created!");
-					resolve();
-				});
-			}));
-
-			
-
-		}
-		Promise.all(promises).then(function () {
-			resolve();
-		});
-	});
-}
-
-function createScreenshots(list, options) {
-	if (list.length > 0) {
-		const shot = list.pop();
-
-		console.log(shot + ' ' + list.length);
-		Jimp.read(__dirname + '/' + shot, function (err, screenshotImg) {
-			if (err) handleError(err, options);
-
-			var promises = [];
-			for (var i in config.screenshotiphone) {
-				var screenshot = config.screenshotiphone[i];
-				var tmpImg = screenshotImg.clone();
-				promises.push(new Promise(function (resolve, reject) {
-					tmpImg.resize(screenshot.width, screenshot.height).write(options.targetPath + "/screenshots/" + screenshot.name + '-' + list.length + ".png", function (err) {
-						if (err) {
-							handleError(err, options);
-						}
-						console.log('Screenshot created');
-						resolve();
-					});
-				}));
-
+		var pos = getCenter(bg.bitmap, image.bitmap);
+		// console.log(options.targetPath + "/" + splash.platform + "/splash/" + splash.name + ".png");
+		bg.composite(image, pos.x, pos.y).write(options.targetPath + "/" + splash.platform + "/splash/" + splash.name + ".png", function (err) {
+			if (err) {
+				handleError(err, options);
+			} else {
+				if (options.progressCB)
+					options.progressCB(splash.name);
+				createSplash(image, doneResolve, options);
 			}
-			Promise.all(promises).then(function () {
-				console.log('Next screenshot');
-				createScreenshots(list, options);
-			});
-			// for(var i in type) {
-			// 	var screenshot = type[i];
 
-			// 	console.log('Creating screenshot: ' + name +' '  +screenshot.width + ', ' + screenshot.height);
-			// 	var tmpImg = image.clone();
-			// 	tmpImg.resize(screenshot.width,screenshot.height).write(options.targetPath+"/screenshots/"+ name + ".png",function(err){
-			// 			if (err) {
-			// 				handleError(err, options);
-			// 				reject();
-			// 			}
-			// 		});
-			// 	if(options.progressCB)
-			// 		options.progressCB(screenshot.name);
-
-			// }
 		});
-	}
-	else {
-		console.log('All screenshots created');
-	}
+	});
+	//console.log(splash.name+" created!");
 }
+
+function createMisc(image, doneResolve, options) {
+	if (config.misc.length == 0) {
+		doneResolve();
+		return;
+	}
+
+	var misc = config.misc.pop();
+
+	new Jimp(misc.width, misc.height, 0xFFFFFFFF, function (err, bg) {
+
+		if (err) handeError(err);
+		var pos = getCenter(bg.bitmap, image.bitmap);
+		bg.composite(image, pos.x, pos.y).write(options.targetPath + "/misc/" + misc.name + ".png", function (err) {
+			if (err) {
+				handleError(err, options);
+			} else {
+				if (options.progressCB)
+					options.progressCB(misc.name);
+				createMisc(image, doneResolve, options);
+			}
+		});
+
+		if (options.progressCB)
+			options.progressCB(misc.name);
+		//console.log(splash.name+" created!");
+	});
+}
+
+// function createScreenshots(list, options) {
+// 	if (list.length > 0) {
+// 		const shot = list.pop();
+
+// 		console.log(shot + ' ' + list.length);
+// 		Jimp.read(__dirname + '/' + shot, function (err, screenshotImg) {
+// 			if (err) handleError(err, options);
+
+// 			var promises = [];
+// 			for (var i in config.screenshotiphone) {
+// 				var screenshot = config.screenshotiphone[i];
+// 				var tmpImg = screenshotImg.clone();
+// 				promises.push(new Promise(function (resolve, reject) {
+// 					tmpImg.resize(screenshot.width, screenshot.height).write(options.targetPath + "/screenshots/" + screenshot.name + '-' + list.length + ".png", function (err) {
+// 						if (err) {
+// 							handleError(err, options);
+// 						}
+// 						console.log('Screenshot created');
+// 						resolve();
+// 					});
+// 				}));
+
+// 			}
+// 			Promise.all(promises).then(function () {
+// 				console.log('Next screenshot');
+// 				createScreenshots(list, options);
+// 			});
+// 			// for(var i in type) {
+// 			// 	var screenshot = type[i];
+
+// 			// 	console.log('Creating screenshot: ' + name +' '  +screenshot.width + ', ' + screenshot.height);
+// 			// 	var tmpImg = image.clone();
+// 			// 	tmpImg.resize(screenshot.width,screenshot.height).write(options.targetPath+"/screenshots/"+ name + ".png",function(err){
+// 			// 			if (err) {
+// 			// 				handleError(err, options);
+// 			// 				reject();
+// 			// 			}
+// 			// 		});
+// 			// 	if(options.progressCB)
+// 			// 		options.progressCB(screenshot.name);
+
+// 			// }
+// 		});
+// 	}
+// 	else {
+// 		console.log('All screenshots created');
+// 	}
+// }
 
 function getCenter(bitmap1, bitmap2) {
 	var pos = {
@@ -214,30 +204,37 @@ module.exports = function (options, doneCallback) {
 		fs.mkdirSync(options.targetPath + "/ios/splash");
 	}
 
-	if (!fs.existsSync(options.targetPath + "/screenshots")) {
-		fs.mkdirSync(options.targetPath + "/screenshots");
-	}
+	// if (!fs.existsSync(options.targetPath + "/screenshots")) {
+	// 	fs.mkdirSync(options.targetPath + "/screenshots");
+	// }
 
-	Jimp.read(options.iconImage, function (err, icon) {
-		if (err) handleError(err, options);
-
-		createIcons(icon, options).then(function () {
-			Jimp.read(options.splashImage, function (err, splash) {
-				if (err) handleError(err, options);
-				createSplash(splash, options).then(function () {
-					if (options.createStoreIcons) {
-						createMisc(splash, options).then(function () {
-							done(options, doneCallback);
-						});
-					} else {
-						done(options, doneCallback);
-					}
-				});
-			});
+	Jimp.read(options.iconImage).then(function (icon) {
+		return new Promise((resolve, reject) => {
+			createIcons(icon, resolve, options);
+		})
+	}).then(function () {
+		console.log('icons done');
+		return Jimp.read(options.splashImage);
+	}).then(function (splash) {
+		return new Promise((resolve, reject) => {
+			createSplash(splash, resolve, options);
 		});
-
+	}).then(function (splash) {
+		console.log('splash done');
+		if (options.createStoreIcons) {
+			new Promise((resolve, reject) => {
+				createMisc(splash, resolve, options)
+			}).then(function () {
+				console.log('misc done');
+				done(options, doneCallback);
+			});
+		} else
+			done(options, doneCallback);
+	}).catch(function (err) {
+		if (err) handleError(err, options);
 	});
 }
+
 
 function done(options, doneCallback) {
 	if (doneCallback)
